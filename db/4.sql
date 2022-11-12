0d3223317492c7a113fbd558c5d07db008800197
gzsig <gaazsig@gmail.com>
2022-11-11 23:16:38 -0300

    db updates

--- db/4.sql
+++ db/4.sql
@@ -2,4 +2,5 @@ CREATE TABLE `lab`.`company` (
   `companyId` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `size` VARCHAR(45) NULL,
-  PRIMARY KEY (`companyId`));
+  PRIMARY KEY (`companyId`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE);
---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO `lab`.`class` (`className`) VALUES ('TECH');
+INSERT INTO `lab`.`class` (`className`) VALUES ('ADM');
+INSERT INTO `lab`.`class` (`className`) VALUES ('PP');
