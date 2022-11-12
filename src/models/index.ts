c5ba4630a8ffd9a71302984f679e3905b6dc936f
gzsig <gaazsig@gmail.com>
2022-11-11 23:17:43 -0300

    types

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,1 +1,22 @@
-export {};
+export interface NewPerson {
+  name: string;
+  lastName: string;
+  birthDate?: Date;
+  email: string;
+  studentId: number;
+  grade: number;
+  salary: number;
+}
+
+export interface Person extends NewPerson {
+  personId: number;
+}
+
+export interface NewCompany {
+  name: string;
+  size: string;
+}
+
+export interface Company extends NewCompany {
+  companyId: number;
+}
