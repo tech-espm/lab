0f0f23d38734ac2d8dfcee21e694f5e5c90ccc18
gzsig <gaazsig@gmail.com>
2022-11-25 10:56:35 -0300

    Throw errors from DAO

--- src/dao/createCompany.ts
+++ src/dao/createCompany.ts
@@ -1,12 +1,9 @@
 import { NewCompany } from "../models";
 import { DAO } from "../utils";
 
-export const createCompany = async (
-  company: NewCompany
-): Promise<number | null> => {
-  try {
-    const upsertRes: any = await DAO.query(
-      `
+export const createCompany = async (company: NewCompany): Promise<number> => {
+  const upsertRes: any = await DAO.query(
+    `
       INSERT INTO lab.company
       (
       name,
@@ -19,27 +16,22 @@ export const createCompany = async (
       size = :size
       ;
       `,
-      company
-    );
+    company
+  );
 
-    console.log({ upsertRes });
+  console.log({ upsertRes });
 
-    if (upsertRes[0]?.insertId) return upsertRes[0].insertId;
+  if (upsertRes[0]?.insertId) return upsertRes[0].insertId;
 
-    const selectRes: any = await DAO.query(
-      `
+  const selectRes: any = await DAO.query(
+    `
       SELECT * FROM lab.company
       WHERE name = :name
       ;
       `,
-      company
-    );
+    company
+  );
 
-    console.log({ selectRes });
-    if (selectRes[0][0]?.name) return selectRes[0][0].companyId;
-    return null;
-  } catch (error) {
-    console.error(String(error));
-    return null;
-  }
+  if (selectRes[0][0]?.name) return selectRes[0][0].companyId;
+  throw "Error creating company";
 };
--- src/dao/createPerson.ts
+++ src/dao/createPerson.ts
@@ -3,10 +3,9 @@ import { DAO } from "../utils";
 
 export const createPerson = async (
   person: NewPerson & { className?: string; companyId?: number }
-): Promise<number | null> => {
-  try {
-    const upsertRes: any = await DAO.query(
-      `
+): Promise<number> => {
+  const upsertRes: any = await DAO.query(
+    `
       INSERT INTO lab.person
       (
       name,
@@ -41,26 +40,20 @@ export const createPerson = async (
       companyId = :companyId
       ;
       `,
-      person
-    );
-    console.log({ person: upsertRes[0] });
+    person
+  );
 
-    if (upsertRes[0]?.insertId) return upsertRes[0].insertId;
+  if (upsertRes[0]?.insertId) return upsertRes[0].insertId;
 
-    const selectRes: any = await DAO.query(
-      `
+  const selectRes: any = await DAO.query(
+    `
       SELECT * FROM lab.person
       WHERE email = :email
       ;
       `,
-      person
-    );
+    person
+  );
 
-    console.log({ selectRes });
-    if (selectRes[0][0]?.name) return selectRes[0][0].personId;
-    return null;
-  } catch (error) {
-    console.error(String(error));
-    return null;
-  }
+  if (selectRes[0][0]?.name) return selectRes[0][0].personId;
+  throw "Error creating person";
 };
